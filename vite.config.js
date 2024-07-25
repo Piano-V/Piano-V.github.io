fd744d382c2bbd14818a14475d0787ec7a632b29
Abhinav Airachia <xabhiz@gmail.com>
2024-07-26 01:12:05 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Piano-v.github.io",
+  base: "/Piano-v.github.io",
   plugins: [react()],
 })
